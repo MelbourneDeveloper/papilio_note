9790f5e45ac419084adbc1b555a65c6fbbe14aaf
Christian Findlay <melbournedeveloper@users.noreply.github.com>
2022-07-18 08:20:33 +1000

    Move the tests

--- src/papilio_note/integration_test/app_tests.dart
+++ src/papilio_note/integration_test/app_tests.dart
@@ -1,7 +1,8 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
-import '../app_test.dart';
-import '../helpers.dart';
+
+import '../test/app_test.dart';
+import '../test/helpers.dart';
 
 void main() {
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
